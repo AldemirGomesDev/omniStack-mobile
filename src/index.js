1689d1d2d300dea785b81fa3b1807e6eeefc4301
aldemir.gomesD <aldemir.gomes@aftisolucoes.com.br>
2019-04-14 18:05:03 -0300

    primeiro commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { YellowBox } from 'react-native';
+
+YellowBox.ignoreWarnings(['Unrecogn']);
+
+import  Routes from './routes';
+
+const App = () => <Routes />;
+
+export default App;
